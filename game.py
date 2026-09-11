b885aff9cb6778abf8097f5f12c1b3159d628bf5
SagePitts14 <sage.m.pitts@gmail.com>
2026-09-11 11:24:33 -0500

    Added New Goblin

--- game.py
+++ game.py
@@ -15,6 +15,10 @@ def main():
     print(f"{goblin.name} enters the arena with {goblin.health} health.")
     print("But no hero has answered the call... yet.")
 
+    goblin = Goblin("Ahstung")
+    
+    print(f"{goblin.name} enters the arena with {goblin.health} health.")
+
 
 if __name__ == "__main__":
     main()
